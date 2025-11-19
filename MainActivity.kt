2a35e7176af1e1c62629575b12d486eca85ed55c
Nova-Gupta <148980981+Nova-Gupta@users.noreply.github.com>
2025-11-19 14:50:00 +0530

    Added layout and kotlin files

---
+++ MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.instagramlinearlayout
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
